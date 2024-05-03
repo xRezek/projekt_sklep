7f543e67a121360897b6c5106a5e9271bedbd275
Mikołaj Maj <136507311+xRezek@users.noreply.github.com>
2024-05-03 21:26:09 +0100

    Add files via upload

--- aboutUs.php
+++ aboutUs.php
@@ -9,58 +9,96 @@
   <title>Projekt</title>
 </head>
   <body>
-    <div class="conrainer-fluid">
-    <nav class="navbar nav-underline navbar-expand-lg bg-body-tertiary fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img class="img-fluid" width="70" length="70" src="./images/logo.png"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-        <li class="nav-item">
-          <a class="nav-link " aria-current="page" href="index.php">Strona Główna</a>
-        </li>
-        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Produkty
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Wszyskie</a></li>
-            <li><a class="dropdown-item" href="#">Kora czołowa</a></li>
-            <li><a class="dropdown-item" href="#">Oczy</a></li>
-            <li><a class="dropdown-item" href="#">Układ krwionośny</a></li>
-            <li><a class="dropdown-item" href="#">Układ immunologinczny</a></li>
-            <li><a class="dropdown-item" href="#">Układ nerwowy</a></li>
-            <li><a class="dropdown-item" href="#">Skóra</a></li>
-            <li><a class="dropdown-item" href="#">System operacyjny</a></li>
-            <li><a class="dropdown-item" href="#">Szkielet</a></li>
-            <li><a class="dropdown-item" href="#">Ręce</a></li>
-            <li><a class="dropdown-item" href="#">Ramiona</a></li>
-            <li><a class="dropdown-item" href="#">Nogi</a></li>
-          </ul>
-          <li class="nav-item ">
-          <a class="nav-link active" href="#">O Nas</a>
-        </li> 
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Kontakt
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="careers.php">Kariera</a></li>
-            <li><a class="dropdown-item" href="#">Wsparcie</a></li>
-          </ul>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    </div>
+    <main class="container-fluid">
+      <header class="container-fluid">
+      <div class="container-fluid">
+        <nav class="navbar nav-underline navbar-expand-lg bg-body-tertiary ">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img class="img-fluid" width="70" length="70" src="./images/logo.png"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+              <ul class="navbar-nav me-auto my-2 my-lg-0 " style="--bs-scroll-height: 100px;">
+                <li class="nav-item">
+                  <a class="nav-link p-2 ms-4" aria-current="page" href="index.php">Strona Główna</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle p-2 ml-4 ms-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Produkty
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Wszyskie</a></li>
+                    <li><a class="dropdown-item" href="#">Kora czołowa</a></li>
+                    <li><a class="dropdown-item" href="#">Oczy</a></li>
+                    <li><a class="dropdown-item" href="#">Układ krwionośny</a></li>
+                    <li><a class="dropdown-item" href="#">Układ immunologinczny</a></li>
+                    <li><a class="dropdown-item" href="#">Układ nerwowy</a></li>
+                    <li><a class="dropdown-item" href="#">Skóra</a></li>
+                    <li><a class="dropdown-item" href="#">System operacyjny</a></li>
+                    <li><a class="dropdown-item" href="#">Szkielet</a></li>
+                    <li><a class="dropdown-item" href="#">Ręce</a></li>
+                    <li><a class="dropdown-item" href="#">Ramiona</a></li>
+                    <li><a class="dropdown-item" href="#">Nogi</a></li>
+                  </ul>
+                <li class="nav-item">
+                  <a class="nav-link active p-2 ms-4" href="aboutUs.php">O Nas</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle p-2 ms-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Kontakt
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="careers.php">Kariera</a></li>
+                    <li><a class="dropdown-item" href="#">Wsparcie</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <div class="nav-item p-2 d-flex"><a href="#" class="nav-link">Zaloguj się</a></div>
+              <div class="nav-item p-2 d-flex"><a href="#" class="nav-link">Zarejestruj się</a></div>
+              <div class="nav-item p-2 d-flex"><a href="#"><button class="d-flex btn-lg btn btn-light text-nowrap btn-outline-dark me-3"><i class="bi bi-bag"></i></button></a></div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      </header>
+    
+      <div class="row">
+        <div class="col pt-2">
+          <img src="./images/_5930f66c-823c-46e8-8ad0-c9cb37188f04.jpg" alt="Modelka" class="img-fluid">
+          <img src="./images/_b8557617-f6bf-4a3b-98d6-4e16559a55e9.jpg " alt="Model" class="img-fluid mt-1">
+            
+        </div>
+        <div class="col-6 pt-2">
+          <div class="row bg-light p-3 rounded-top">
+            <div class="col text-center h1">
+              Odkryj Swoją Modową Wizję z Naszym Sklepem Odzieżowym!
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="h3">Twój Styl, Nasza Pasja: Najnowsze Trendy dla Ciebie!</div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <hr class="border-2 bg-black">
+              <p class="display-6">
+                Jesteśmy sklepem odzieżowym, gdzie moda łączy się z wygodą, a styl staje się osobistą wizytówką każdego klienta. Nasza misja to nie tylko dostarczanie najnowszych trendów, ale także inspiracja do wyrażania siebie poprzez ubrania. Z pasją selekcjonujemy najwyższej jakości produkty, które nie tylko podkreślą Twój indywidualny styl, ale również sprawią, że będziesz się czuł wyjątkowo. Niezależnie od okazji, nasz sklep jest miejscem, w którym każdy może znaleźć coś dla siebie - od klasycznych klasyków po odważne nowości. Dołącz do naszej społeczności modowej i odkryj, jak odzież może zmieniać świat i Twoje życie!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col pt-2">
+          <img src="./images/_4787f272-331e-468c-a7e2-70c5ca7ec429.jpg" alt="model" class="img-fluid">
+          <img src="./images/_724368c0-0cf9-4d4f-b02c-3edbd2ae4092.jpg" alt="Modelka" class="img-fluid mt-1">
+        </div>
+      </div>
+    </main>
+    <footer class="container-fluid">
+
+    </footer>
     
 
     <script src="./scripts/bootstrap.bundle.min.js"></script>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pl-PL">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,121 +8,163 @@
   <link rel="stylesheet" href="./css/bootstrap.min.css">
   <link rel="stylesheet" href="./css/style.css">
   <link rel="stylesheet" href="./css/bootstrap-icons.min.css">
-  
   <title>Projekt</title>
 </head>
-  <body>
+
+<body>
+  <header class="container-fluid">
     <div class="container-fluid">
-      <nav class="navbar nav-underline navbar-expand-lg bg-body-tertiary ">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img class="img-fluid" width="70" length="70" src="./images/logo.png"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarScroll">
-              <ul class="navbar-nav me-auto my-2 my-lg-0 " style="--bs-scroll-height: 100px;">
-                <li class="nav-item">
-                  <a class="nav-link active p-2 ms-4" aria-current="page" href="#">Strona Główna</a>
-                </li>
-                
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle p-2 ml-4 ms-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Produkty
-                  </a>
-                  <ul class="dropdown-menu" >
-                    <li><a class="dropdown-item" href="#">Wszyskie</a></li>
-                    <li><a class="dropdown-item" href="#">Kora czołowa</a></li>
-                    <li><a class="dropdown-item" href="#">Oczy</a></li>
-                    <li><a class="dropdown-item" href="#">Układ krwionośny</a></li>
-                    <li><a class="dropdown-item" href="#">Układ immunologinczny</a></li>
-                    <li><a class="dropdown-item" href="#">Układ nerwowy</a></li>
-                    <li><a class="dropdown-item" href="#">Skóra</a></li>
-                    <li><a class="dropdown-item" href="#">System operacyjny</a></li>
-                    <li><a class="dropdown-item" href="#">Szkielet</a></li>
-                    <li><a class="dropdown-item" href="#">Ręce</a></li>
-                    <li><a class="dropdown-item" href="#">Ramiona</a></li>
-                    <li><a class="dropdown-item" href="#">Nogi</a></li>
-                  </ul>
-                  <li class="nav-item">
-                  <a class="nav-link p-2 ms-4" href="aboutUs.php">O Nas</a>
-                </li> 
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle p-2 ms-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Kontakt
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="careers.php">Kariera</a></li>
-                    <li><a class="dropdown-item" href="#">Wsparcie</a></li>
-                  </ul>
-                </li>   
-              </ul>
-              <div class="nav-item p-2 d-flex"><a href="#" class="nav-link">Zaloguj się</a></div>
-              <div class="nav-item p-2 d-flex"><a href="#" class="nav-link">Zarejestruj się</a></div>
+      <nav class="navbar nav-underline navbar-expand-lg bg-white ">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img class="img-fluid" width="70" length="70" src="./images/_2c6136cf-ef31-4a72-af0d-4300fe95d5b9.jpg"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 " style="--bs-scroll-height: 100px;">
+              <li class="nav-item">
+                <a class="nav-link active p-2 ms-4" aria-current="page" href="#">Strona Główna</a>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle p-2 ml-4 ms-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Produkty
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Wszyskie</a></li>
+                  <li><a class="dropdown-item" href="#">Kora czołowa</a></li>
+                  <li><a class="dropdown-item" href="#">Oczy</a></li>
+                  <li><a class="dropdown-item" href="#">Układ krwionośny</a></li>
+                  <li><a class="dropdown-item" href="#">Układ immunologinczny</a></li>
+                  <li><a class="dropdown-item" href="#">Układ nerwowy</a></li>
+                  <li><a class="dropdown-item" href="#">Skóra</a></li>
+                  <li><a class="dropdown-item" href="#">System operacyjny</a></li>
+                  <li><a class="dropdown-item" href="#">Szkielet</a></li>
+                  <li><a class="dropdown-item" href="#">Ręce</a></li>
+                  <li><a class="dropdown-item" href="#">Ramiona</a></li>
+                  <li><a class="dropdown-item" href="#">Nogi</a></li>
+                </ul>
+              <li class="nav-item">
+                <a class="nav-link p-2 ms-4" href="aboutUs.php">O Nas</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle p-2 ms-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Kontakt
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="careers.php">Kariera</a></li>
+                  <li><a class="dropdown-item" href="#">Wsparcie</a></li>
+                </ul>
+              </li>
+            </ul>
+            <div class="nav-item p-2 d-flex"><a href="#" class="nav-link">Zaloguj się</a></div>
+            <div class="nav-item p-2 d-flex"><a href="#" class="nav-link">Zarejestruj się</a></div>
+            <div class="nav-item p-2 d-flex"><a href="#"><button class="d-flex btn-lg btn btn-light text-nowrap btn-outline-dark me-3"><i class="bi bi-bag"></i></button></a></div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!-- main -->
+  <main class="container p-3">
+    <div class="row gx-4">
+      <div class="col-2 bg-light shadow-lg rounded">jfkadsh</div>
+      
+      <div class="col-8 bg-light shadow-lg rounded">
+        <div class="row ">
+          <div class="col h2">Co nowego?</div>
+        </div>
+        
+        <div class="row p-2">  
+          <div class="col-5 mx-5 card rounded shadow">
+            <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+              <h5> <a href="#" class="text-dark">Awesome product</a></h5>
+              <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </div>
           </div>
-          <div class="nav-item"><a href="#"><button class="d-flex btn-lg btn btn-light text-nowrap btn-outline-dark me-3"><i class="bi bi-bag"></i></button></a></div>
-        </nav>
-    </div> 
-    <!-- main -->
-    <div class="container mt-1">
-      <div class="row"><div class="col h1"><h1 class="h1 text-center">Ulepsz swoje ciało</h1></div></div>
-      <div class="row">
-        <section class="col-2 shadow-lg p-1 me-4 bg-body-tertiary rounded text-bg-light">ajdfslkahskj</section>
-        <section class = "col-6 shadow-lg p-4 bg-body-tertiary rounded text-bg-light">
-        <div class="row"><div class="col h2"> <h2 class="h2">Co nowego?</h2></div></div>
-          <div class="row">
-            <div class="col">
-              <div class="card mt-1" style="width: 18rem;">
-                  <img src="./images/_c44d644b-901d-43ef-a11b-1062bf9907f8.jpg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>  
-              </div>
-              <div class="col">
-                <div class="card mt-1" style="width: 18rem;">
-                  <img src="./images/_e27617fd-a0b0-40ce-acc8-a0d0f936017a.jpg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
+          <div class="col-5 card rounded shadow">
+            <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+              <h5> <a href="#" class="text-dark">Awesome product</a></h5>
+              <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </div>
           </div>
-          <div class="row">
-            <div class="col">
-              <div class="card mt-1" style="width: 18rem;">
-                  <img src="./images/_c44d644b-901d-43ef-a11b-1062bf9907f8.jpg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>  
-              </div>
-              <div class="col">
-                <div class="card mt-1" style="width: 18rem;">
-                  <img src="./images/_c44d644b-901d-43ef-a11b-1062bf9907f8.jpg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
+        </div>
+        <div class="row p-2">  
+          <div class="col-5 mx-5 card rounded shadow">
+            <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+              <h5> <a href="#" class="text-dark">Awesome product</a></h5>
+              <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </div>
           </div>
-        </section> 
-        <section class="col-3 shadow-lg p-3 ms-4 bg-body-tertiary rounded text-bg-light">dshfslkajk</section>
+          <div class="col-5 card rounded shadow">
+            <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+              <h5> <a href="#" class="text-dark">Awesome product</a></h5>
+              <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row p-2">  
+          <div class="col-5 mx-5 card rounded shadow">
+            <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+              <h5> <a href="#" class="text-dark">Awesome product</a></h5>
+              <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div class="col-5 card rounded shadow">
+            <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+              <h5> <a href="#" class="text-dark">Awesome product</a></h5>
+              <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row p-2">  
+          <div class="col-5 mx-5 card rounded shadow">
+            <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+              <h5> <a href="#" class="text-dark">Awesome product</a></h5>
+              <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div class="col-5 card rounded shadow">
+            <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+              <h5> <a href="#" class="text-dark">Awesome product</a></h5>
+              <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row p-2">  
+          <div class="col-5 mx-5 card rounded shadow">
+            <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+              <h5> <a href="#" class="text-dark">Awesome product</a></h5>
+              <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div class="col-5 card rounded shadow">
+            <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+              <h5> <a href="#" class="text-dark">Awesome product</a></h5>
+              <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col-2  bg-light shadow-lg rounded">ajlsdfk</div>
+    </div>
+  </main>
+  <footer class="container-fluid">
+    <div class="row bg-black text-light">
+      <div class="col">
+        fdgdhsiodfhkjliduoghk
       </div>
     </div>
+  
+  </footer>
 
-    
 
-    <script src="./scripts/bootstrap.bundle.min.js"></script>
-    <script src="./scripts/app.js"></script>
-    <script src="./scripts/jquery-3.7.1.min.js"></script>
-  </body>
+
+
+  <script src="./scripts/bootstrap.bundle.min.js"></script>
+  <script src="./scripts/app.js"></script>
+  <script src="./scripts/jquery-3.7.1.min.js"></script>
+</body>
+
 </html>
